a0e2a7e8a339be562bd0d54b408ea6a94b720918
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-11-24 19:37:17 -0500

    real-time classes

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ new Vue({
       name: 'Bitcoin',
       img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
       changePercent: 0,
+
+      price: 8200,
+
       prices: [8400, 7900, 8200, 9000, 9400, 10000, 102000],
       pricesWithDays: [
         { day: 'Lunes', value: 8400 },

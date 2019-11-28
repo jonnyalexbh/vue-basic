a49dd322e18069784965c8b92e28766b8138ff5e
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-11-27 23:10:30 -0500

    two-way data binding

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ new Vue({
       img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
       changePercent: 0,
 
+      value: 0,
       color: 'f4f4f4',
 
       price: 8200,
@@ -27,6 +28,12 @@ new Vue({
   computed: {
     title() {
       return `${this.name} - ${this.symbol}`
+    },
+    convertedValue() {
+      if (!this.value) {
+        return 0
+      }
+      return this.value / this.price
     }
   },
   watch: {
